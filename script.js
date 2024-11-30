7b023969731b7de34f04594721997909b1bcd861
Mi13285 <mixkud95@inbox.ru>
2024-11-30 10:14:55 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function tribonacci(signature, n) {
+  if (n >= 3) {
+    return signature.slice(0, n);
+  } else {
+    const s = tribonacci(signature, n - 1);
+    return [...s, s[s.length - 1] + s[s.length - 2] + s[s.length - 3]];
+  }
+}
+console.log(tribonacci([1, 1, 1], 7));
